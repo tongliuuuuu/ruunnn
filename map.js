91c36e308aaf313df75ec4e5d736c1a3b64aa81a
tongliuuuuu <44879864+tongliuuuuu@users.noreply.github.com>
2018-12-20 00:11:20 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,91 +1,182 @@
-<html>
-  <head>
-    <title>It's time to Ruunnnn!</title>
 
-<!--link to mapbox gl js [library]-->
-    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.50.0/mapbox-gl.js'></script>
+// Initialize map
+mapboxgl.accessToken = 'pk.eyJ1IjoibGVtb245MzAyIiwiYSI6ImNpdTNsNW83MTBoZG0yb2xrdXpwcXR6cTMifQ.mxXMrYp7gRrbTS125K3RiQ'; // replace this value with your own access token from Mapbox Studio
 
-<!--link to jquery library-->
-    <script
-     src="https://code.jquery.com/jquery-3.3.1.min.js"
-    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-    crossorigin="anonymous"></script>
+var map = new mapboxgl.Map({
+    container: 'map', // this is the ID of the div in index.html where the map should go
+    center: [-78.526668,38.036223], // set the centerpoint of the map programatically. Note that this is [longitude, latitude]!
+    zoom: 12.6, // set the default zoom programatically
+    style: 'mapbox://styles/lemon9302/cjnc4htrd0bxn2sqeqbtwyesb', // replace this value with the style URL from Mapbox Studio
+    customAttribution: 'City of Charlottesville Open Data Portal (http://opendata.charlottesville.org/)', // Custom text used to attribute data source(s)
+});
 
-<!--link to mapbox gl js css style sheet-->
-      <!--The href attribute specifies the URL of the page the link goes to-->
-    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.50.0/mapbox-gl.css' rel='stylesheet' />
 
-<!-- Link to our custom stylesheet -->  
-  <link rel="stylesheet" href="style.css">
 
-<!-- Link to Google font library -->    
-  <link rel='stylesheet' type='text/css'
-href='http://fonts.googleapis.com/css?family=Condiment'>
-  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+// Show modal when About button is clicked
+$("#about").on('click', function() { // Click event handler for the About button in jQuery, see https://api.jquery.com/click/
+    $("#screen").fadeToggle(); // shows/hides the black screen behind modal, see https://api.jquery.com/fadeToggle/
+    $(".modal").fadeToggle(); // shows/hides the modal itself, see https://api.jquery.com/fadeToggle/
+});
+
+$(".modal>.close-button").on('click', function() { // Click event handler for the modal's close button
+    $("#screen").fadeToggle();
+    $(".modal").fadeToggle();
+});
+
+map.on('load', function() {
+    $('.modal-text').slideToggle('slow');
+
+});
+
+
+// Legend
+var layers = [ // an array of the possible values you want to show in your legend
+    'Civic Spaces',
+    'Community Park',
+    'Neighborhood Park',
+    'Cemetery',
+    'Urban Park',
+    'Regional Park'
+];
+
+var colors = [ // an array of the color values for each legend item
+    '#800000',
+    '#800030',
+    '#800060',
+    '#80006c',
+    '#800090',
+    '#80009c'
+];
+
+// for loop to create individual legend items
+for (i=0; i<layers.length; i++) {
+    var layer =layers[i]; // name of the current legend item, from the layers array
+    var color =colors[i]; // color value of the current legend item, from the colors array 
+    
+    var itemHTML = "<div><span class='legend-key'></span><span>" + layer + "</span></div>"; // create the HTML for the legend element to be added
+    var item = $(itemHTML).appendTo("#legend"); // add the legend item to the legend
+    var legendKey = $(item).find(".legend-key"); // find the legend key (colored circle) for the current item
+    legendKey.css("background", color); // change the background color of the legend key
+}
+
+// 10.25 starts here----------------------------------------------
+// INFO WINDOW CODE 
+    // Create a popup on click 
+    map.on('click', function(e) {   // Event listener to do some code when user clicks on the map
+
+      var places = map.queryRenderedFeatures(e.point, {  // Query the map at the clicked point. See https://www.mapbox.com/mapbox-gl-js/example/queryrenderedfeatures/ for an example on how queryRenderedFeatures works and https://www.mapbox.com/mapbox-gl-js/api/#map#queryrenderedfeatures for documentation
+        layers: ['cville-aptpt']    // replace this with the name of the layer from the Mapbox Studio layers panel
+    });
+
+      // if the layer is empty, this if statement will exit the function (no popups created) -- this is a failsafe to avoid non-functioning popups
+      if (places.length == 0) {
+        $('#info-window').slideUp();
+        return;
+    }
+        $('#info-window').slideDown();
+          $('#address').html(places[0].properties.Address);
+          $('#Info').html(places[0].properties.Info);
+          
+          
+
+             map.flyTo({
+             center: [routes[0].properties.Corx, routes[0].properties.Cory],
+             zoom: 15,
+             curve: 1,
+             bearing:20,
+             pitch:15
+             });
   
-  </head>
+
+        $("#zoomin").on('click', function() { 
+        window.open('www.zillow.com/'+ places[0].properties.Url)
+   });
+
+        $("#clear").on('click', function() { 
+              var layers = [
+             '42596',
+             '29850',
+             '82826',
+             '111106',
+             '175965',
+             '181243',
+             '182953',
+             '193459',
+             '198182',
+             '206724',
+             '208997',
+             '217645',
+             '217738',
+             '334911',
+             '369827',
+             '250787053',
+             '251445567',
+             '251447145',
+             '251892957',
+              ]
+              
+
+              for (i=0; i<layers.length; i++) {
+              map.setLayoutProperty(layers[i], 'visibility', 'none');
+              };
+
+             map.flyTo({
+             center: [-78.526668,38.036223],
+             zoom: 12.6,
+             bearing: 0,
+             pitch:0,
+             });
+             
+
+        });
+    });
+    
+        map.on('mouseenter','cville-aptpt', function(e) {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+        
+        var places = map.queryRenderedFeatures(e.point, {  // Query the map at the clicked point. See https://www.mapbox.com/mapbox-gl-js/example/queryrenderedfeatures/ for an example on how queryRenderedFeatures works and https://www.mapbox.com/mapbox-gl-js/api/#map#queryrenderedfeatures for documentation
+        layers: ['cville-aptpt']    // replace this with the name of the layer from the Mapbox Studio layers panel
+    });
+
+      // if the layer is empty, this if statement will exit the function (no popups created) -- this is a failsafe to avoid non-functioning popups
+
+    // Initiate the popup
+    var popup = new mapboxgl.Popup({ 
+        closeButton: true, // If true, a close button will appear in the top right corner of the popup. Default = true
+        closeOnClick: true, // If true, the popup will automatically close if the user clicks anywhere on the map. Default = true
+        anchor: 'bottom', // The popup's location relative to the feature. Options are 'top', 'bottom', 'left', 'right', 'top-left', 'top-right', 'bottom-left' and 'bottom-right'. If not set, the popup's location will be set dynamically to make sure it is always visible in the map container.
+        offset: [0, -15] // A pixel offset from the centerpoint of the feature. Can be a single number, an [x,y] coordinate, or an object of [x,y] coordinates specifying an offset for each of the different anchor options (e.g. 'top' and 'bottom'). Negative numbers indicate left and up.
+    });
+
+      // Set the popup location based on each feature
+      popup.setLngLat(places[0].geometry.coordinates);
+
+      // Set the contents of the popup window
+      popup.setHTML('<h2>' + places[0].properties.Name + '</h2><h3>$' + places[0].properties.Price + '+</h3>');
+            // stops[0].properties.stop_id will become the title of the popup (<h3> element)
+            // stops[0].properties.stop_name will become the body of the popup
+
+      // Add the popup to the map 
+      popup.addTo(map);  // replace "map" with the name of the variable in line 4, if different
+
+       map.on('mouseleave', 'cville-aptpt', function() {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
+  });
+      
 
 
+$("#about").on('click', function() { 
+  map.flyTo({
+             center: [-78.526668,38.036223],
+             zoom: 12.6,
+             bearing: 0,
+             pitch:0,
+             speed:0.5,
+             });
 
-  <body>
-    <!-- About this map button -->
-    <button id="about" class='button-default'>Ruunnn!</button>
+  $('#info-window').slideUp();
 
-    <div class="modal1" >
-         <div class='modal-text'>
-         <h1 class='modal1-title'>Rental places in walkble distance for A-school students </h1>
-
-         <p class='modal-body'>
-          Use this map to check all possible rental spaces in 10, 20, 25 min walking distance to UVA A-school! It's time to walk to school!!!
-         </p>
-                    <div id='modal1-hint' style= 'display:inline;'><p> Click for details!</p></div>
-         </div>
-    </div>
-     <!-- Layers control (populated by javascript code) -->
-    <div id='layers-control'></div>
-   
-
-  <div class='map-overlay'  id='info-window'>
-    <div>
-          <div style="height: 50px;">
-           <h2>Rental information</h2>
-           <button id="zoomin" class='button-zoom' >Go!</button>
-           <button id="clear" class='button-clear' >CL</button>
-          </div>
-          <table border="0" cellpadding="0">
-        <thead></thead>
-        <tfoot></tfoot>
-        <tbody>
-            <tr style="margin-bottom: 20px">
-                <td style="vertical-align: top; width: 250px; height: auto; "> 
-                    <h4>Address:</h4>
-                    <p style="margin-top: 10px;" class="notranslate">
-                        <span class="large_number" id="address" style = "font-size:15px; color:#000;">n/a</span>
-                        <span style="font-size: 12px; color: #fff; font-weight: 400; margin-left: 10px;">
-                        
-                        </span>
-                    </p>
-                </td>
-
-                <td style="border-left: 1px solid #fff; padding-left: 20 px; vertical-align: top; width: 300px;">
-                    <h4 style="padding-left:10px;">Details</h4>
-                        <span class="large_number" id="Info" style = "padding-left:10px; vertical-align: middle; font-size:15px; color:#000;">n/a</span>
-                    </p>
-                </td>
-
-            </tr>
-        </tbody>
-    </table>
-
-    </div>
-  </div>
-  
-
-  
-  <div id="map"></div>
-   
-
-  </body>
-  <!-- Link to our custom javascript, must be placed at the end of the index.html file -->  
-  <script src="map.js"></script>
-</html>
+}); 
